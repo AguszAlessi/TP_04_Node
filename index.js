870e9132cd5789d1617550d91b92646922d48dff
AGUSTIN ZAID ALESSI <48385003@ort>
2025-04-04 12:10:18 -0300

    ejercicio 1 terminado

---
+++ index.js
@@ -0,0 +1,10 @@
+//EJERCICIO 1
+
+const concatenar = require('./modulos/string')
+
+console.log(concatenar("hola" , " soy agus"))
+
+//EJERCICIO 2
+
+const matematica = require('./modulos/matematica')
+
